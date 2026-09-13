3b5cdfffcb2f63e8028c808e1ad619eea62107ca
hujaafar <hjawady703@gmail.com>
2026-09-14 01:01:17 +0300

    feat(ratings): bootstrap the rating microservice

---
+++ rating-service/src/main/java/io/neo4flix/rating/RatingApplication.java
@@ -0,0 +1,12 @@
+package io.neo4flix.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "io.neo4flix")
+public class RatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingApplication.class, args);
+    }
+}
